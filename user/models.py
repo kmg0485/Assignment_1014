a36c41ff3e984609a6faac81d84536f599a313d5
kmg0485 <kmg0485@gmail.com>
2022-10-14 17:26:53 +0900

    fin

---
+++ user/models.py
@@ -0,0 +1,9 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class User(AbstractUser):
+    phone   = models.TextField(max_length=500, blank = True)
+    address = models.TextField(max_length=500, blank = True)
